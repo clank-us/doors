3148deb6d63d3c2d3a140f8ed867113fc6f173f6
Micah Cooper <mrmicahcooper+git@gmail.com>
2026-08-03 14:10:00 -0500

    Open up

---
+++ lib/door.ex
@@ -0,0 +1,18 @@
+defmodule Door do
+  @moduledoc """
+  Documentation for `Door`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Door.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
