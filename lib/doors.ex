936ef738215d68a129845297aa926adda47200bf
Micah Cooper <mrmicahcooper+git@gmail.com>
2026-08-04 13:32:18 -0500

    Remove generated code

--- lib/doors.ex
+++ lib/doors.ex
@@ -1,18 +1,2 @@
 defmodule Doors do
-  @moduledoc """
-  Documentation for `Doors`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Doors.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/doors_test.exs
+++ test/doors_test.exs
@@ -1,8 +1,2 @@
 defmodule DoorsTest do
-  use ExUnit.Case
-  doctest Doors
-
-  test "greets the world" do
-    assert Doors.hello() == :world
-  end
 end
